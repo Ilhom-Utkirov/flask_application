b7f2c26bc8e4eea9ff397658496a80d3d8d22328
Ilhom Utkirov <inspiration9595@gmail.com>
2023-04-01 17:49:21 +0200

    ttt

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'base/basic.html'
+def index():  # put application's code here
+    return render_template('base/basic.html')
+
+
+@app.route('/info')
+def info():  # put application's code here
+    return "<h1>Cute Puppy</h1>"
+
+
+@app.route('/puppy/<name>')
+def puppy(name):
+    return "<h1>page for {} </h1>".format(name)
+
+
+@app.route('/puppy_upper/<name>')
+def puppy_upper(name):
+    return "<h1>upper {} </h1>".format(name.upper())
+
+
+@app.route('/puppy_2nd_letter/<name>')
+def puppy_2nd_letter(name):
+    return "<h1>2nd letter {} </h1>".format(name[2])
+
+
+@app.route('/puppy_name_latin/<name>')
+def puppylatin(name):
+    puppyname = ''
+
+    if name[-1] == 'y':
+        puppyname = name[:-1] + 'iful'
+    else:
+        puppyname = name + 'y'
+    return "<h1> pupy name = {}".format(puppyname)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
